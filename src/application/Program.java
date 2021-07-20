1bbe161f09a717d1fe4936fe5bf0ba056e334cca
Gervasio Teixeira <gervasio.teixeira@gmail.com>
2021-07-19 21:43:58 -0300

    implementacao insert seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -12,24 +13,29 @@ public class Program {
 	public static void main(String[] args) {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
 		System.out.println("=== TESTE 1: Encontrar vendedor por ID ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
+
 		System.out.println("\n=== TESTE 2: Encontrar vendedor por Departamento ===");
-		Department dep = new Department(2,null);
+		Department dep = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(dep);
-		
-		for(Seller s : list) {
+
+		for (Seller s : list) {
 			System.out.println(s);
 		}
-		
+
 		System.out.println("\n=== TESTE 3: Encontrar todos os vendedores ===");
 		list = sellerDao.findAll();
-		for(Seller s : list) {
+		for (Seller s : list) {
 			System.out.println(s);
 		}
-	}
 
+		System.out.println("\n=== TESTE 4: Inserir vendedor ===");
+		Seller newSeller = new Seller(null, "Greg", "gerg@gmail.com", new Date(), 4000.0, dep);
+
+		sellerDao.insert(newSeller);
+		System.out.println("Vendedor inserido! Id= " + newSeller.getId());
+	}
 }
