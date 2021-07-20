71b009c614cefbd49d89c27252332312a96cfc91
Gervasio Teixeira <gervasio.teixeira@gmail.com>
2021-07-19 21:08:40 -0300

    implementacao encontrar vendedor por departamento

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -9,10 +12,17 @@ public class Program {
 	public static void main(String[] args) {
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println("=== TESTE 1: Encontrar vendedor por ID ===");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
+		
+		System.out.println("\n=== TESTE 2: Encontrar vendedor por Departamento ===");
+		Department dep = new Department(2,null);
+		List<Seller> list = sellerDao.findByDepartment(dep);
+		for(Seller s : list) {
+			System.out.println(s);
+		}
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -94,6 +97,48 @@ public class SellerDaoJDBC implements SellerDao{
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department dep) {
+		
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st=conn.prepareStatement("SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name");
+			
+			st.setInt(1, dep.getId());
+			
+			rs = st.executeQuery();
+			List <Seller> list = new ArrayList<>();
+			Map<Integer, Department> map =new HashMap<>(); //Para controlar a não repetição do departamento (para não instanciar um departamento diferente para cada instância de funcionario)
+			
+			while(rs.next()) {
+				Department depmap = map.get(rs.getInt("DepartmentID"));//verifica se no map já existe o departamento cadastrado (busca pelo id que vem pelo ResultSet)
+				
+				if(depmap==null) {
+					depmap=instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentID"), depmap);
+				}
+				
+				Seller seller = instantiateSeller(rs,depmap);
+				
+				list.add(seller);
+				
+			}
+			return list;
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
 	}
 
 }
